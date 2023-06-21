12878c639c5d941ce5b7c98def74f6ad6f7c82a5
mcspe <mcspezzaferro@gmail.com>
2023-06-21 23:26:44 +0200

    [add] type in project seeder

--- config/projects.php
+++ config/projects.php
@@ -2,21 +2,21 @@
   return [
     [
       'title'=>'spotify web',
-      'preview'=>'resources\img\spotify-web.png',
+      'preview'=>'uploads/2023/06/spotify-web.png',
       'summary'=>'Create a responsive graphic version of Spotify Web Player',
       'languages'=>'html|css|css-mediaquery',
       'link'=>'https://github.com/mcspe/html-css-spotifyweb'
     ],
     [
       'title'=>'image carousel',
-      'preview'=>'resources\img\array-carousel.png',
+      'preview'=>'uploads/2023/06/array-carousel.png',
       'summary'=>'Create a carousel display for a set of images using vanilla js',
       'languages'=>'html|css|js',
       'link'=>'https://github.com/mcspe/js-array-carousel'
     ],
     [
       'title'=>'Boolflix',
-      'preview'=>'resources\img\boolflix.png',
+      'preview'=>'uploads/2023/06/boolflix.png',
       'summary'=>'Recreate the popular streaming web app',
       'languages'=>'html|scss|js|vue',
       'link'=>'https://github.com/mcspe/vite-boolflix'
--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Project;
+use App\Models\Type;
 use App\Helpers\FunctionHelper;
 
 
@@ -21,9 +22,10 @@ class ProjectsTableSeeder extends Seeder
       // dd($projects);
       foreach ($projects as $project) {
         $new_project = new Project();
+        $new_project->type_id = Type::inRandomOrder()->first()->id;
         $new_project->title = $project['title'];
-        $new_project->slug = FunctionHelper::generateUniqueSlug($new_project->title, new Project);
-        // $new_project->preview = $project['preview'];
+        $new_project->slug = FunctionHelper::generateUniqueSlug($new_project->title, new Project());
+        $new_project->image_path = $project['preview'];
         $new_project->summary = $project['summary'];
         $new_project->languages = $project['languages'];
         $new_project->link = $project['link'];
